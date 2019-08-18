13d4ee053d81c39ff2e41ce18f5c481bb00e1929
麦文俊 <429839446@qq.com>
2019-08-18 17:15:08 +0800

    log
    11

--- main/main.go
+++ main/main.go
@@ -87,6 +87,13 @@ func printVersion() {
 }
 
 func main() {
+	f, err := os.OpenFile("/var/log/v2ray/logfile.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("file open error : %v", err)
+	}
+	defer f.Close()
+	log.SetOutput(f)
+	
 	flag.Parse()
 
 	printVersion()
